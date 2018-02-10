d32945b8f5e52698e70bb588d76e672929118943
Nir Karl <nir@yosigal.com>
2018-02-10 23:24:57 +0200

    added user buttons for single rotations and others (uncommented)

--- rubikarl.py
+++ rubikarl.py
@@ -61,28 +61,26 @@ UNSLEEP = {"U": 23, "R": 26, "F": 19, "D": 13, "L": 6, "B": 5}
 CW = 1
 CCW = 0
 SPR = 200
+MODE = (14, 15, 18)
+RESOLUTION = {'Full': (0, 0, 0),
+    'Half': (1, 0, 0),
+    '1/4': (0, 1, 0),
+    '1/8': (1, 1, 0),
+    '1/16': (0, 0, 1),
+    '1/32': (1, 0, 1)}
+step_count = SPR * 32
+delay = 0.0104 / 32
 
 def pi_init():
     None
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(DIR, GPIO.OUT)
-    # GPIO.setup(STEP, GPIO.OUT)
-    # for u in UNSLEEP:
-    #     GPIO.setup(UNSLEEP[u], GPIO.OUT)
-    # GPIO.output(DIR, CW)
-    #
-    # MODE = (14, 15, 18)
-    # GPIO.setup(MODE, GPIO.OUT)
-    # RESOLUTION = {'Full': (0, 0, 0),
-    # 'Half': (1, 0, 0),
-    # '1/4': (0, 1, 0),
-    # '1/8': (1, 1, 0),
-    # '1/16': (0, 0, 1),
-    # '1/32': (1, 0, 1)}
-    # GPIO.output(MODE, RESOLUTION['1/32'])
-    #
-    # step_count = SPR * 32
-    # delay = 0.0104 / 32
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(DIR, GPIO.OUT)
+    GPIO.setup(STEP, GPIO.OUT)
+    for u in UNSLEEP:
+        GPIO.setup(UNSLEEP[u], GPIO.OUT)
+    GPIO.output(DIR, CW)
+    GPIO.setup(MODE, GPIO.OUT)
+    GPIO.output(MODE, RESOLUTION['1/32'])
 
 pi_init()
 
@@ -90,32 +88,31 @@ UNSLEEP_gpio = [UNSLEEP[u] for u in UNSLEEP]
 
 def rotation(unsleep, direction):
     None
-    #     gpios = {23: "U", 26: "R", 19: "F", 13: "D", 6: "L", 5: "B"}
-    #     GPIO.output(unsleep, GPIO.HIGH)
-    #     GPIO.output(DIR, direction)
-    #     print("Face: {1} - {2} | direction: {3}".format(unsleep, gpios[unsleep], direction))
-    #     for i in range(step_count):
-    #         GPIO.output(STEP, GPIO.HIGH)
-    #         sleep(delay)
-    #         GPIO.output(STEP, GPIO.LOW)
-    #         sleep(delay)
-    #     GPIO.output(unsleep, GPIO.LOW)
+    gpios = {23: "U", 26: "R", 19: "F", 13: "D", 6: "L", 5: "B"}
+    GPIO.output(unsleep, GPIO.HIGH)
+    GPIO.output(DIR, direction)
+    print("Face: {1} - {2} | direction: {3}".format(unsleep, gpios[unsleep], direction))
+    for i in range(step_count):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
+    GPIO.output(unsleep, GPIO.LOW)
 
 
 def pi():
     None
-    # commands = re.split(r'\s*', solution)
-    # for i in commands:
-    #     if i.__len__() == 1:
-    #         rotation(UNSLEEP[i], CW)
-    #     if i.__len__() == 2:
-    #         if i[1] == "'":
-    #             print(UNSLEEP[i[0]])
-    #             rotation(UNSLEEP[i[0]], CCW)
-    #         else:
-    #             rotation(UNSLEEP[i[0]], CW)
-    #             rotation(UNSLEEP[i[0]], CW)
-    #
+    commands = re.split(r'\s*', solution)
+    for i in commands:
+        if i.__len__() == 1:
+            rotation(UNSLEEP[i], CW)
+        if i.__len__() == 2:
+            if i[1] == "'":
+                print(UNSLEEP[i[0]])
+                rotation(UNSLEEP[i[0]], CCW)
+            else:
+                rotation(UNSLEEP[i[0]], CW)
+                rotation(UNSLEEP[i[0]], CW)
 
 
 def background():
