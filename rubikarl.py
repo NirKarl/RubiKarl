6d3d6c46f023caf19868f360203d7958615ac3fa
Nir Karl <nir@yosigal.com>
2018-02-28 13:26:43 +0200

    step count fixed

--- rubikarl.py
+++ rubikarl.py
@@ -75,7 +75,7 @@ RESOLUTION = {'Full': (0, 0, 0),
               '1/8': (1, 1, 0),
               '1/16': (0, 0, 1),
               '1/32': (1, 0, 1)}
-step_count = int(SPR/4)
+step_count = int(SPR)
 delay = 0.0104 / 32
 
 def changeResolution(resolution):
