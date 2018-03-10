e7eb2f32651e4118971160a71f5643f03da5c60c
Nir Karl <nir@yosigal.com>
2018-03-10 14:33:52 +0200

    step count fixed

--- rubikarl.py
+++ rubikarl.py
@@ -69,7 +69,7 @@ UNSLEEP = {"U": 11, "R": 26, "F": 19, "D": 13, "L": 6, "B": 5}
 CW = 1
 CCW = 0
 SPR = 200  # step per revolution
-res = '1/32'
+res = 'Full'
 MODE = (14, 15, 18)
 RESOLUTION = {'Full': (0, 0, 0),
               'Half': (1, 0, 0),
@@ -77,7 +77,7 @@ RESOLUTION = {'Full': (0, 0, 0),
               '1/8': (1, 1, 0),
               '1/16': (0, 0, 1),
               '1/32': (1, 0, 1)}
-step_count = int(SPR/4)+2
+step_count = int(SPR/4)+1
 delay = 0.1 / 32
 
 def changeResolution(resolution):
