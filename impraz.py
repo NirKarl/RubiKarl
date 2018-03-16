fdd1cead1fc4d4919d2c0d8d51f154adccc09b48
Nir <nir@yosigal.com>
2018-03-16 22:07:05 +0200

    need some color tuning (show normal pic)

--- impraz.py
+++ impraz.py
@@ -23,18 +23,28 @@ recColors = {WHITE: (255, 255, 255),
              ORANGE: (0, 125, 255)
              }
 
-imgHeight = 480/2
-imgWidth = 640/2
-recSize = 60
-recPos = [((int(imgWidth - recSize * 2.5 - recSize / 2), int(imgHeight - recSize * 2.5 - recSize / 2)), (int(imgWidth - recSize * 2.5 + recSize / 2), int(imgHeight - recSize * 2.5 + recSize / 2))),
-          ((int(imgWidth - recSize / 2), int(imgHeight - recSize * 2.5 - recSize / 2)), (int(imgWidth + recSize / 2), int(imgHeight - recSize * 2.5 + recSize / 2))),
-          ((int(imgWidth + recSize * 2.5 - recSize / 2), int(imgHeight - recSize * 2.5 - recSize / 2)), (int(imgWidth + recSize * 2.5 + recSize / 2), int(imgHeight - recSize * 2.5 + recSize / 2))),
-          ((int(imgWidth - recSize * 2.5 - recSize / 2), int(imgHeight - recSize / 2)), (int(imgWidth - recSize * 2.5 + recSize / 2), int(imgHeight + recSize / 2))),
-          ((int(imgWidth - recSize / 2), int(imgHeight - recSize / 2)), (int(imgWidth + recSize / 2), int(imgHeight + recSize / 2))),
-          ((int(imgWidth + recSize * 2.5 - recSize / 2), int(imgHeight - recSize / 2)), (int(imgWidth + recSize * 2.5 + recSize / 2), int(imgHeight + recSize / 2))),
-          ((int(imgWidth - recSize * 2.5 - recSize / 2), int(imgHeight + recSize * 2.5 - recSize / 2)), (int(imgWidth - recSize * 2.5 + recSize / 2), int(imgHeight + recSize * 2.5 + recSize / 2))),
-          ((int(imgWidth - recSize / 2), int(imgHeight + recSize * 2.5 - recSize / 2)), (int(imgWidth + recSize / 2), int(imgHeight + recSize * 2.5 + recSize / 2))),
-          ((int(imgWidth + recSize * 2.5 - recSize / 2), int(imgHeight + recSize * 2.5 - recSize / 2)), (int(imgWidth + recSize * 2.5 + recSize / 2), int(imgHeight + recSize * 2.5 + recSize / 2)))]
+imgHeight = 480 / 2
+imgWidth = 640 / 2
+recSize = 55
+recPos = [((int(imgWidth - recSize * 2.5 - recSize / 2), int(imgHeight - recSize * 2.5 - recSize / 2)),
+           (int(imgWidth - recSize * 2.5 + recSize / 2), int(imgHeight - recSize * 2.5 + recSize / 2))),
+          ((int(imgWidth - recSize / 2), int(imgHeight - recSize * 2.5 - recSize / 2)),
+           (int(imgWidth + recSize / 2), int(imgHeight - recSize * 2.5 + recSize / 2))),
+          ((int(imgWidth + recSize * 2.5 - recSize / 2), int(imgHeight - recSize * 2.5 - recSize / 2)),
+           (int(imgWidth + recSize * 2.5 + recSize / 2), int(imgHeight - recSize * 2.5 + recSize / 2))),
+          ((int(imgWidth - recSize * 2.5 - recSize / 2), int(imgHeight - recSize / 2)),
+           (int(imgWidth - recSize * 2.5 + recSize / 2), int(imgHeight + recSize / 2))),
+          ((int(imgWidth - recSize / 2), int(imgHeight - recSize / 2)),
+           (int(imgWidth + recSize / 2), int(imgHeight + recSize / 2))),
+          ((int(imgWidth + recSize * 2.5 - recSize / 2), int(imgHeight - recSize / 2)),
+           (int(imgWidth + recSize * 2.5 + recSize / 2), int(imgHeight + recSize / 2))),
+          ((int(imgWidth - recSize * 2.5 - recSize / 2), int(imgHeight + recSize * 2.5 - recSize / 2)),
+           (int(imgWidth - recSize * 2.5 + recSize / 2), int(imgHeight + recSize * 2.5 + recSize / 2))),
+          ((int(imgWidth - recSize / 2), int(imgHeight + recSize * 2.5 - recSize / 2)),
+           (int(imgWidth + recSize / 2), int(imgHeight + recSize * 2.5 + recSize / 2))),
+          ((int(imgWidth + recSize * 2.5 - recSize / 2), int(imgHeight + recSize * 2.5 - recSize / 2)),
+           (int(imgWidth + recSize * 2.5 + recSize / 2), int(imgHeight + recSize * 2.5 + recSize / 2)))]
+
 
 def getColor(h, s, v):
     if s in range(0, 60):  # (0, 90)
@@ -52,6 +62,7 @@ def getColor(h, s, v):
             else:
                 return RED
 
+
 def getRectColor(frame, p1, p2, drawFrame):
     cube_frame = frame[p1[0]:p2[0], p1[1]:p2[1]].reshape(-1, 3).T
     h = numpy.median(cube_frame[0])
@@ -61,22 +72,56 @@ def getRectColor(frame, p1, p2, drawFrame):
     drawRec(drawFrame, (p1, p2), recColors[color])
     return color
 
+
 def drawRec(img, pos, color, fill=False):
     thickness = 3
     if fill:
         thickness = -1
     square = cv2.rectangle(img, pos[0], pos[1], color, thickness)  # BGR
     return square
+
+
 # camera = input("enter the camera number: ")
+
+cHSV = {
+    "blue": {"min": [179, 255, 255], "max": [0, 0, 0]},
+    "white": {"min": [179, 255, 255], "max": [0, 0, 0]},
+    "yellow": {"min": [179, 255, 255], "max": [0, 0, 0]},
+    "red": {"min": [179, 255, 255], "max": [0, 0, 0]},
+    "orange": {"min": [179, 255, 255], "max": [0, 0, 0]},
+    "green": {"min": [179, 255, 255], "max": [0, 0, 0]}
+}
+
+
+def calibrate(frame, p1, p2, color):
+    cube_frame = frame[p1[0]:p2[0], p1[1]:p2[1]].reshape(-1, 3).T
+    global cHSV
+    for i in range(0, 3):
+        if numpy.median(cube_frame[i]) < cHSV[color]["min"][i]:
+            cHSV[color]["min"][i] = numpy.median(cube_frame[i])
+        elif numpy.median(cube_frame[i]) > cHSV[color]["max"][i]:
+            cHSV[color]["max"][i] = numpy.median(cube_frame[i])
+
+
 cap = cv2.VideoCapture(1)
+count = 0
 while True:
     ret, frameRGB = cap.read()
     ret, frameHSV = cap.read()
-    cv2.cvtColor(frameHSV, cv2.COLOR_BGR2HSV, frameHSV)
+    cv2.cvtColor(frameRGB, cv2.COLOR_BGR2HSV, frameHSV)
+    if count < 115 and count > 15:
+        for p in recPos:
+            calibrate(frameHSV, p[0], p[1], "blue")
+
+    elif count == 115:
+        print(cHSV["blue"])
+
+    count += 1
+
     for p in recPos:
         color = getRectColor(frameHSV, p[0], p[1], frameRGB)
         # print(color)
-    # cv2.imshow('HSV', frameHSV)
+    cv2.imshow('HSV', frameHSV)
     cv2.imshow('RGB', frameRGB)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
